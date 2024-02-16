a0483bda687a6c5a5a37b685a9b21953fcfde229
Andrew Gunn <andrewgunn31@gmail.com>
2024-02-16 09:11:09 -0600

    added base stuff for mongodb and connection is working

---
+++ src/SearchService/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService.Models;
+
+// This class represents the Item entity in the database and is used to map the Item collection in the database.
+public class Item : Entity
+{
+	public int ReservePrice { get; set; }
+	public string Seller { get; set; }
+	public string Winner { get; set; }
+	public int SoldAmount { get; set; }
+	public int CurrentHighBid { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public DateTime UpdatedAt { get; set; }
+	public DateTime AuctionEnd { get; set; }
+	public string Status { get; set; }
+	public string Make { get; set; }
+	public string Model { get; set; }
+	public int Year { get; set; }
+	public string Color { get; set; }
+	public int Mileage { get; set; }
+	public string ImageUrl { get; set; }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,8 +16,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +25,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb",
+	MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+await DB.Index<Item>()
+	.Key(x => x.Make, KeyType.Text)
+	.Key(x => x.Model, KeyType.Text)
+	.Key(x => x.Color, KeyType.Text)
+	.CreateAsync();
 
 app.Run();
