c26f2b27545e74212e096274ae72b81896d3fb1d
Andrew Gunn <andrewgunn31@gmail.com>
2024-02-19 14:34:27 -0600

    Allowing reg/login of user and returning an access_token in postman

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Account.Register;
+
+public class RegisterViewModel
+{
+	[Required] public string Email { get; set; }
+
+	[Required] public string Password { get; set; }
+
+	[Required] public string Username { get; set; }
+
+	[Required] public string FullName { get; set; }
+	public string ReturnUrl { get; set; }
+	public string Button { get; set; }
+}
