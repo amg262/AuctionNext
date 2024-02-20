080228c901d0572f9118ceed1826a59642d70aaa
Andrew Gunn <andrewgunn31@gmail.com>
2024-02-19 19:55:32 -0600

    adding consumers for future bidding and finishing auctions

---
+++ src/Contracts/AuctionFinished.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts;
+/// <summary>
+/// Represents the completion state of an auction, indicating whether an item was sold, to whom, and for what amount.
+/// </summary>
+public class AuctionFinished
+{
+	public bool ItemSold { get; set; }
+	public string AuctionId { get; set; }
+	public string Winner { get; set; }
+	public string Seller { get; set; }
+	public int? Amount { get; set; }
+}
---
+++ src/Contracts/BidPlaced.cs
@@ -0,0 +1,14 @@
+﻿namespace Contracts;
+
+/// <summary>
+/// Represents a bid placed on an auction, including the bidder's information, bid amount, and the bid's status.
+/// </summary>
+public class BidPlaced
+{
+	public string Id { get; set; } // Id of the bid
+	public string AuctionId { get; set; }
+	public string Bidder { get; set; }
+	public DateTime BidTime { get; set; }
+	public int Amount { get; set; }
+	public string BidStatus { get; set; }
+}
