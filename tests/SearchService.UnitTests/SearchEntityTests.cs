56b76c81ac94cf655114e65c8873d2217f8a5cbd
Andrew Gunn <andrewgunn31@gmail.com>
2024-02-23 13:31:48 -0600

    added some more unit tests, one with parameters

--- tests/SearchService.UnitTests/SearchEntityTests.cs
+++ tests/SearchService.UnitTests/SearchEntityTests.cs
@@ -29,4 +29,75 @@ public class SearchEntityTests
 		// Assert: Verify that the method returns true for Item with a created date.
 		Assert.True(response);
 	}
+
+	[Fact]
+	public void CreatedAt_IsDateTime()
+	{
+		// Arrange
+		var item = new Item {CreatedAt = DateTime.Now};
+
+		// Act
+		var createdAt = item.CreatedAt;
+
+		// Assert
+		Assert.IsType<DateTime>(createdAt);
+	}
+
+	[Fact]
+	public void CreatedAt_IsNotFutureDate_True()
+	{
+		// Arrange
+		var item = new Item {CreatedAt = DateTime.Now};
+
+		// Act
+		var createdAt = item.CreatedAt;
+
+		// Assert
+		Assert.True(createdAt <= DateTime.Now);
+	}
+
+
+	[Fact]
+	public void Year_InvalidYearInt_ReturnsTrue()
+	{
+		// Arrange
+		var item = new Item {Year = 2021};
+
+		// Act
+		var year = item.Year.ToString();
+
+		// Assert
+		Assert.IsNotType<int>(year);
+	}
+
+	[Fact]
+	public void Year_InvalidYearRange_False()
+	{
+		// Arrange: Create a Item instance with a year.
+		var item = new Item {Year = 2026};
+
+		// Act: Invoke the method under test.
+		var currentYear = DateTime.Now.Year;
+		var minYear = currentYear - 100;
+		var maxYear = ++currentYear;
+
+		// Assert: Verify that the method returns false for Item with a year outside the valid range.
+		Assert.NotInRange(item.Year, minYear, maxYear);
+	}
+
+	[Theory]
+	[InlineData(1900)]
+	[InlineData(1999)]
+	[InlineData(2024)]
+	public void Year_WithinValidRange_IsValid(int year)
+	{
+		// Arrange: Create a Item instance with a year.
+		var item = new Item {Year = year};
+
+		// Act: Invoke the method under test.
+		var isValidYear = item.Year >= 1900 && item.Year <= DateTime.Now.Year;
+
+		// Assert: Verify that the method returns true for Item with a year within the valid range.
+		Assert.True(isValidYear);
+	}
 }
