3bd2cceee2678533beace39aa9d3d5c931e82b66
Andrew Gunn <andrewgunn31@gmail.com>
2024-02-24 12:12:39 -0600

    fixing the docker build issue from UnitTests and have API json data displaying on page for Search

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
 
-export default function Listings() {
+async function getData() {
+  const res = await fetch('http://localhost:6001/search')
+
+  if (!res.ok) throw new Error('Failed to fetch data');
+
+  return res.json();
+}
+
+export default async function Listings() {
+  const data = await getData();
+
   return (
-      <div>asdfa</div>
+      <div>{JSON.stringify(data, null)}</div>
   );
 }
--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,13 +1,12 @@
 // import Image from "next/image";
-'use client'
-export default function Home() {
 
+import Listings from "@/app/auctions/Listings";
+
+export default function Home() {
   console.log('Home');
   return (
       <div>
-        <h3 className='text-3xl'>
-          Content goes here.
-        </h3>
+        <Listings/>
       </div>
   );
 }
