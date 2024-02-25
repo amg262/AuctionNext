dfaa17ceb23f4206a65ccd19be0e7ab86cb9667b
Andrew Gunn <andrewgunn31@gmail.com>
2024-02-25 10:44:03 -0600

    created types and displaying images

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import Image from "next/image";
 import {CountdownTimer} from "@/app/auctions/CountdownTimer";
+import {CarImage} from "@/app/auctions/CarImage";
+import {Auction} from "@/types";
 
 type Props = {
-  auction: any;
+  auction: Auction;
 }
 
 export default function AuctionCard({auction}: Props) {
   return (
-      <a href="#">
+      <a href="#" className="group ">
         <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
 
           <div>
-            <Image
-                src={auction.imageUrl}
-                alt={auction.make}
-                fill={true}
-                priority={true}
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
-            />
+            <CarImage imageUrl={auction.imageUrl}/>
             <div className="absolute bottom-2 left-2">
-              <CountdownTimer auctionEnd={auction.auctionEnd} />
+              <CountdownTimer auctionEnd={auction.auctionEnd}/>
             </div>
           </div>
         </div>
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,23 +1,45 @@
 import * as React from 'react';
 import AuctionCard from "@/app/auctions/AuctionCard";
+import {Auction, PagedResult} from "@/types";
 
-async function getData() {
+/**
+ * Asynchronously fetches auction data from the server.
+ *
+ * This function sends a GET request to a predefined URL, expecting to fetch a paged list of auction items.
+ * It handles the network response, ensuring that any non-OK response throws an error.
+ *
+ * @return {Promise<PagedResult<Auction>>} A promise that resolves to a paged result containing auction items.
+ * @throws {Error} When the fetch operation fails or the response status is not OK.
+ */
+async function getData(): Promise<PagedResult<Auction>> {
+  // Sends a fetch request to the server to get auction data.
   const res = await fetch('http://localhost:6001/search?pageSize=10');
 
+  // Checks if the response was not OK and throws an error.
   if (!res.ok) throw new Error('Failed to fetch data');
 
+  // Parses the JSON response and returns it as a promise.
   return res.json();
 }
 
-export default async function Listings() {
+/**
+ * The Listings component asynchronously fetches auction listings and renders them using AuctionCard components.
+ *
+ * This component makes use of the `getData` function to fetch data on mount and then renders a grid of AuctionCard
+ * components, each representing an auction. The grid layout is achieved using Tailwind CSS classes.
+ *
+ * @return {Promise<JSX.Element>} A promise that resolves to the JSX.Element representing the listings grid.
+ */
+export default async function Listings(): Promise<JSX.Element> {
+  // Fetches auction data using the getData function.
   const data = await getData();
 
+  // Renders the auction data as a grid of AuctionCard components.
   return (
       <div className="grid grid-cols-4 gap-6">
-        {data && data.results.map((auction: any) => (
+        {data && data.results.map(auction => (
             <AuctionCard key={auction.id} auction={auction}/>
         ))}
-        {/*{JSON.stringify(data, null, 2)}*/}
       </div>
   );
 }
