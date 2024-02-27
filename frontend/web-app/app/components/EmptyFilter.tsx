7b79d530471b66eb4524e874ab28dd99d73a968c
Andrew Gunn <andrewgunn31@gmail.com>
2024-02-27 08:41:20 -0600

    fixed the Identity login page issue

--- frontend/web-app/app/components/EmptyFilter.tsx
+++ frontend/web-app/app/components/EmptyFilter.tsx
@@ -1,16 +1,23 @@
+'use client';
+
 import {useParamsStore} from "@/hooks/useParamsStore";
 import {Button} from "flowbite-react";
 import Heading from "@/app/components/Heading";
+import {signIn} from "next-auth/react";
 
 type Props = {
   title?: string;
   subtitle?: string;
   showReset?: boolean;
+  showLogin?: boolean;
+  callbackUrl?: string;
 }
 export default function EmptyFilter({
                                       title = "No matches for this filter",
                                       subtitle = "Try changing or reseting filter",
-                                      showReset
+                                      showReset,
+                                      showLogin,
+                                      callbackUrl
                                     }: Props) {
 
   const reset = useParamsStore(state => state.reset);
@@ -21,6 +28,9 @@ export default function EmptyFilter({
         <div className='mt-4'>
           {showReset && (
               <Button outline onClick={reset}>Remove Filters</Button>
+          )}
+          {showLogin && (
+              <Button outline onClick={() => signIn('id-server', {callbackUrl})}>Login</Button>
           )}
         </div>
       </div>
---
+++ frontend/web-app/middleware.ts
@@ -0,0 +1,11 @@
+export {default} from "next-auth/middleware"
+
+export const config = {
+  matcher: [
+    '/session'
+  ],
+  pages: {
+    signIn: '/api/auth/signin',
+    redirect: '/session',
+  },
+}
