e47fa8a062c89afe375976e641b19fb3fc05a3aa
Andrew Gunn <andrewgunn31@gmail.com>
2024-03-05 10:30:02 -0600

    handling the IP address issue with the identity svc

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -9,66 +9,63 @@ namespace IdentityService;
 
 internal static class HostingExtensions
 {
-    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddRazorPages();
+	public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddRazorPages();
 
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-        builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
+		builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+			.AddEntityFrameworkStores<ApplicationDbContext>()
+			.AddDefaultTokenProviders();
 
-        builder.Services
-            .AddIdentityServer(options =>
-            {
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
+		builder.Services
+			.AddIdentityServer(options =>
+			{
+				options.Events.RaiseErrorEvents = true;
+				options.Events.RaiseInformationEvents = true;
+				options.Events.RaiseFailureEvents = true;
+				options.Events.RaiseSuccessEvents = true;
 
-                if (builder.Environment.IsEnvironment("Docker"))
-                {
-                    options.IssuerUri = "identity-svc";
-                }
+				if (builder.Environment.IsEnvironment("Docker"))
+				{
+					options.IssuerUri = "identity-svc";
+				}
 
-                // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
-                // options.EmitStaticAudienceClaim = true;
-            })
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
-            .AddAspNetIdentity<ApplicationUser>()
-            .AddProfileService<CustomProfileService>();
+				// see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+				// options.EmitStaticAudienceClaim = true;
+			})
+			.AddInMemoryIdentityResources(Config.IdentityResources)
+			.AddInMemoryApiScopes(Config.ApiScopes)
+			.AddInMemoryClients(Config.Clients(builder.Configuration))
+			.AddAspNetIdentity<ApplicationUser>()
+			.AddProfileService<CustomProfileService>();
 
-        builder.Services.ConfigureApplicationCookie(options => 
-        {
-            options.Cookie.SameSite = SameSiteMode.Lax;
-        });
-        
-        builder.Services.AddAuthentication();
+		builder.Services.ConfigureApplicationCookie(options => { options.Cookie.SameSite = SameSiteMode.Lax; });
 
-        return builder.Build();
-    }
-    
-    public static WebApplication ConfigurePipeline(this WebApplication app)
-    { 
-        app.UseSerilogRequestLogging();
-    
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
+		builder.Services.AddAuthentication();
 
-        app.UseStaticFiles();
-        app.UseRouting();
-        app.UseIdentityServer();
-        app.UseAuthorization();
-        
-        app.MapRazorPages()
-            .RequireAuthorization();
+		return builder.Build();
+	}
 
-        return app;
-    }
+	public static WebApplication ConfigurePipeline(this WebApplication app)
+	{
+		app.UseSerilogRequestLogging();
+
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
+
+		app.UseStaticFiles();
+		app.UseRouting();
+		app.UseIdentityServer();
+		app.UseAuthorization();
+
+		app.MapRazorPages()
+			.RequireAuthorization();
+
+		return app;
+	}
 }
