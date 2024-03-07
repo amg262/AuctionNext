2f3010c20f57510681c2abc27cf995d703b715ed
Andrew Gunn <andrewgunn31@gmail.com>
2024-03-07 09:03:24 -0600

    fixing the issue with access to the diagnostics page

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -9,18 +9,22 @@ namespace IdentityService.Pages.Diagnostics;
 [Authorize]
 public class Index : PageModel
 {
-    public ViewModel View { get; set; }
-        
-    public async Task<IActionResult> OnGet()
-    {
-        var localAddresses = new string[] { "::ffff:172.18.0.1", "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-        {
-            return NotFound();
-        }
+	public ViewModel View { get; set; }
 
-        View = new ViewModel(await HttpContext.AuthenticateAsync());
-            
-        return Page();
-    }
+	public async Task<IActionResult> OnGet()
+	{
+		var localAddresses = new string[]
+		{
+			"::ffff:172.18.0.1", "127.0.0.1", "::1", "146.190.34.72", "::ffff:10.5.0.200", "::ffff:10.5.0.7",
+			HttpContext.Connection.LocalIpAddress.ToString()
+		};
+		if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+		{
+			return NotFound();
+		}
+
+		View = new ViewModel(await HttpContext.AuthenticateAsync());
+
+		return Page();
+	}
 }
