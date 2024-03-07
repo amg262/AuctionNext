4a819dc50a29296633fca1e2da883a924d538aac
Andrew Gunn <andrewgunn31@gmail.com>
2024-03-07 14:11:00 -0600

    allowing the correct IP

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -16,6 +16,7 @@ public class Index : PageModel
 		var localAddresses = new string[]
 		{
 			"::ffff:172.18.0.1", "127.0.0.1", "::1", "146.190.34.72", "::ffff:10.5.0.200", "::ffff:10.5.0.7",
+			"::ffff:172.19.0.4:80", "::ffff:172.19.0.2",
 			HttpContext.Connection.LocalIpAddress.ToString()
 		};
 		if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
