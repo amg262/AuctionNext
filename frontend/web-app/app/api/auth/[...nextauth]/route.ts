873d9ca041a3e37298f64137f8889828ae634913
Andrew Gunn <andrewgunn31@gmail.com>
2024-03-09 14:00:29 -0600

    making it work in docker

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,7 @@ export const authOptions: NextAuthOptions = {
     DuendeIdentityServer6({
       id: 'id-server',
       clientId: 'nextApp',
-      clientSecret: process.env.CLIENT_SECRET!,
+      clientSecret: "secret",
       issuer: process.env.ID_URL,
       authorization: {params: {scope: 'openid profile auctionApp'}},
       idToken: true
