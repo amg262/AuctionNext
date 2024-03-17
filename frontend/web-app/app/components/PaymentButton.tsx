7fed1a4a81de2413a1f07a7f235aa9136fac9b8a
Andrew Gunn <andrewgunn31@gmail.com>
2024-03-17 14:32:53 -0500

    improving the paymentButton to function a little more React-like

--- frontend/web-app/app/components/PaymentButton.tsx
+++ frontend/web-app/app/components/PaymentButton.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {loadStripe} from '@stripe/stripe-js';
 import {createPayment, getPayment} from "@/app/actions/auctionActions";
-import {Button} from "flowbite-react";
 
 type Props = {
   // id: string;
@@ -16,11 +15,24 @@ type Props = {
 
 
 export default function PaymentButton({data}: Props) {
-  const stripePromise = loadStripe(
-      process.env.NEXTAUTH_STRIPE_SECRET || 'pk_test_51NKl9DJCh47a7Nh113trEHxzNk32tOgF5qtNOCfO2Jb5Hc7D8lC7kY4pVm6L7cQkaY5di4VNw0UPAuGrMSb4e9XB00NfTT04a5');
+  const [stripeError, setStripeError] = React.useState(null);
+  const [stripe, setStripe] = React.useState<any>();
+  // const stripePromise = loadStripe(
+  //     process.env.NEXTAUTH_STRIPE_SECRET || 'pk_test_51NKl9DJCh47a7Nh113trEHxzNk32tOgF5qtNOCfO2Jb5Hc7D8lC7kY4pVm6L7cQkaY5di4VNw0UPAuGrMSb4e9XB00NfTT04a5');
 
+  useEffect(() => {
+    const initializeStripe = async () => {
+      if (!stripe) {
+        const stripeTmp = await loadStripe('pk_test_51NKl9DJCh47a7Nh113trEHxzNk32tOgF5qtNOCfO2Jb5Hc7D8lC7kY4pVm6L7cQkaY5di4VNw0UPAuGrMSb4e9XB00NfTT04a5');
+        setStripe(stripeTmp);
+      }
+    };
+
+    initializeStripe().then(r => console.log(r)).catch(e => console.error(e));
+  }, [stripe]);
   const handleCheckout = async () => {
-    const stripe = await stripePromise as any;
+    setStripeError(null);
+    // const stripe = await stripePromise as any;
 
     const res = await getPayment()
     console.log(res);
@@ -33,16 +45,32 @@ export default function PaymentButton({data}: Props) {
     // Redirect to Stripe Checkout
     const result = await stripe.redirectToCheckout({
       sessionId: response.session.id,
-    });
+    }).then(function (result: any) {
+      if (result.error) {
+        setStripeError(result.error.message);
+      }
 
-    if (result.error) {
-      alert(result.error.message);
-    }
+    });
   };
 
   return (
-      <Button onClick={handleCheckout}>
-        Make Payment
-      </Button>
+      // <Button onClick={handleCheckout}>
+      //   Make Payment
+      // </Button>
+      <>
+        {stripe ? (
+            <button
+                className="bg-indigo-600 text-white px-3 py-2 rounded text-base border-0"
+                id="checkout-button-price_1Heree568gerg54rtretrt"
+                role="link"
+                type="button"
+                onClick={handleCheckout}
+            >
+              Checkout
+            </button>
+        ) : "Loading..."
+        }
+        {stripeError ? <div id="error-message">{stripeError}</div> : null}
+      </>
   );
 }
