cb1b960ac38e706ddf974c5e3a6fd3d0460d12fc
Andrew Gunn <andrewgunn31@gmail.com>
2024-03-21 09:13:09 -0500

    added MAUI Blazor Hybrid project for future mobile app using .NET

---
+++ frontend/BlazorMobile/BlazorMobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorMobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ frontend/BlazorMobile/BlazorMobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorMobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
