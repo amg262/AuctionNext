24a8dbb4ef2f3ad7eda63e2883aca84567565d06
Andrew Gunn <andrewgunn31@gmail.com>
2024-04-22 20:09:51 -0500

    added new accounts

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -4,44 +4,48 @@ namespace IdentityService;
 
 public static class Config
 {
-	public static IEnumerable<IdentityResource> IdentityResources =>
-		new IdentityResource[]
-		{
-			new IdentityResources.OpenId(),
-			new IdentityResources.Profile(),
-		};
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
 
-	public static IEnumerable<ApiScope> ApiScopes =>
-		new ApiScope[]
-		{
-			new ApiScope("auctionApp", "Auction app full access"),
-		};
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new("auctionApp", "Auction app full access"),
+        };
 
-	public static IEnumerable<Client> Clients(IConfiguration config) =>
-		new Client[]
-		{
-			new Client
-			{
-				ClientId = "postman",
-				ClientName = "Postman",
-				AllowedScopes = {"openid", "profile", "auctionApp"},
-				RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
-				ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
-				AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
-			},
-			new Client
-			{
-				ClientId = "nextApp",
-				ClientName = "nextApp",
-				ClientSecrets = {new Secret(config["ClientSecret"].Sha256())},
-				AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
-				RequirePkce = false,
-				// RedirectUris = {config["ClientApp"] + "https://app.auctionnext.com/api/auth/callback/id-server"},
-				RedirectUris = {"https://app.auctionnext.com/api/auth/callback/id-server", config["ClientApp"] + "/api/auth/callback/id-server"},
-				AllowOfflineAccess = true,
-				AllowedScopes = {"openid", "profile", "auctionApp"},
-				AccessTokenLifetime = 3600 * 24 * 30,
-				AlwaysIncludeUserClaimsInIdToken = true
-			}
-		};
+    public static IEnumerable<Client> Clients(IConfiguration config) =>
+        new Client[]
+        {
+            new()
+            {
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                RedirectUris = { "https://www.getpostman.com/oauth2/callback" },
+                ClientSecrets = new[] { new Secret("NotASecret".Sha256()) },
+                AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
+            },
+            new()
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = { new Secret(config["ClientSecret"].Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                // RedirectUris = {config["ClientApp"] + "https://app.auctionnext.com/api/auth/callback/id-server"},
+                RedirectUris =
+                {
+                    "https://app.auctionnext.com/api/auth/callback/id-server",
+                    config["ClientApp"] + "/api/auth/callback/id-server"
+                },
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                AccessTokenLifetime = 3600 * 24 * 30,
+                AlwaysIncludeUserClaimsInIdToken = true
+            }
+        };
 }
--- src/IdentityService/SeedData.cs
+++ src/IdentityService/SeedData.cs
@@ -35,13 +35,15 @@ public class SeedData
                 throw new Exception(result.Errors.First().Description);
             }
 
-            result = userMgr.AddClaimsAsync(alice, new Claim[]{
-                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
-                        }).Result;
+            result = userMgr.AddClaimsAsync(alice, new Claim[]
+            {
+                new(JwtClaimTypes.Name, "Alice Smith"),
+            }).Result;
             if (!result.Succeeded)
             {
                 throw new Exception(result.Errors.First().Description);
             }
+
             Log.Debug("alice created");
         }
         else
@@ -64,9 +66,10 @@ public class SeedData
                 throw new Exception(result.Errors.First().Description);
             }
 
-            result = userMgr.AddClaimsAsync(bob, new Claim[]{
-                            new Claim(JwtClaimTypes.Name, "Bob Smith"),
-                        }).Result;
+            result = userMgr.AddClaimsAsync(bob, new Claim[]
+            {
+                new(JwtClaimTypes.Name, "Bob Smith"),
+            }).Result;
             if (!result.Succeeded)
             {
                 throw new Exception(result.Errors.First().Description);
@@ -77,6 +80,69 @@ public class SeedData
         else
         {
             Log.Debug("bob already exists");
+        }
+
+        var tom = userMgr.FindByNameAsync("tom").Result;
+        if (tom == null)
+        {
+            tom = new ApplicationUser
+            {
+                UserName = "tom",
+                Email = "TomSmith@email.com",
+                EmailConfirmed = true,
+            };
+            var result = userMgr.CreateAsync(tom, "Pass123$").Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = userMgr.AddClaimsAsync(tom, new Claim[]
+            {
+                new(JwtClaimTypes.Name, "Tom Smith"),
+            }).Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            Log.Debug("tom created");
+        }
+        else
+        {
+            Log.Debug("tom already exists");
+        }
+
+        var andrew = userMgr.FindByNameAsync("andrew").Result;
+        if (andrew == null)
+        {
+            andrew = new ApplicationUser
+            {
+                UserName = "andrew",
+                Email = "andrewgunn31@gmail.com",
+                EmailConfirmed = true
+            };
+            var result = userMgr.CreateAsync(andrew, "Pass123$").Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = userMgr.AddClaimsAsync(andrew, new Claim[]
+            {
+                new(JwtClaimTypes.Name, "Andrew Gunn"),
+                new(JwtClaimTypes.Email, "andrewgunn31@gmail.com"),
+            }).Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            Log.Debug("andrew created");
+        }
+        else
+        {
+            Log.Debug("andrew already exists");
         }
     }
 }
