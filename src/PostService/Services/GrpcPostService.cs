95528d901a88eebe3887e3e47882df524cef8d48
Andrew Gunn <andrewgunn31@gmail.com>
2024-05-01 19:38:46 -0500

    Update Docker and gRPC configurations, protobuf file changes.
    - Updated Docker config to use different ports
    - Adjusted gRPC endpoints in appsettings files
    - Refactored protobuf file structure for posts services

--- src/PostService/Services/GrpcPostService.cs
+++ src/PostService/Services/GrpcPostService.cs
@@ -1,7 +1,34 @@
-﻿namespace PostService.Services;
+﻿using MongoDB.Entities;
+
+namespace PostService.Services;
+
+using Grpc.Core;
+using PostService;
 
 public class GrpcPostService
 {
-    // I need to create the method names in the proto file and pull the data from the database
-    // 
+    public GrpcPostService()
+    {
+    }
+
+    public async Task<GrpcPostResponse> GetPost(GetPostRequest request, ServerCallContext context)
+    {
+        Console.WriteLine("==> Received Grpc request for post");
+
+        var post = await DB.Find<Post>().OneAsync(Guid.Parse((ReadOnlySpan<char>)request.Id))
+                   ?? throw new RpcException(new Status(StatusCode.NotFound, "Not found"));
+
+        var response = new GrpcPostResponse
+        {
+            Post = new GrpcPostModel
+            {
+                Id = post.Id.ToString(),
+                UserId = post.UserId,
+                Content = post.Content,
+                Title = post.Title
+            }
+        };
+
+        return response;
+    }
 }
